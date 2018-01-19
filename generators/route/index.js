c4f5fbde2cfbdba619c8abe2189a6be82e45cb6e
ashinzekene <ashinzekene@gmail.com>
2018-01-19 03:21:04 +0100

    first commir

---
+++ generators/route/index.js
@@ -0,0 +1,33 @@
+const Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+  constructor(args, opt) {
+    super(args, props)
+    
+  }
+
+  prompting() {
+    return this.prompt([{
+      type    : 'input',
+      name    : 'name',
+      message : 'Your project name',
+      default : this.appname // Default to current folder name
+    }, {
+      type    : 'confirm',
+      name    : 'cool',
+      message : 'Would you like to enable the Cool feature?'
+    }]).then((answers) => {
+      this.log('app name', answers.name);
+      this.log('cool feature', answers.cool);
+    });
+  }
+  
+  method1() {
+    this.log('method 1 just ran');
+  }
+
+  method2() {
+    this.log('method 2 just ran');
+  }
+  
+};

5ca8f1887dd2a11f12796c33e1d9c2ade73d07f2
ashinzekene <ashinzekene@gmail.com>
2018-01-24 05:58:29 +0100

    added kebabToConstant

--- utils/case-change.js
+++ utils/case-change.js
@@ -1,3 +1,4 @@
 module.exports.kebabToCamel = (str) => str.split("-").map((a,b) => [...a].map((c,d) => d === 0 && b!==0 ? c.toUpperCase() : c.toLowerCase()).join('')).join('')
 module.exports.kebabToPascal = (str) => str.split("-").map((a) => [...a].map((b, c) => c === 0 ? b.toUpperCase() : b.toLowerCase()).join('')).join('')
+module.exports.kebabToConstant = str =>[...str].map(s=> s === " " || s == "-" ? "_" : s.toUpperCase()).join("")
 module.exports.toLower = (str) => [...str].filter((s) => s !== "-" && s !== " ").join('')

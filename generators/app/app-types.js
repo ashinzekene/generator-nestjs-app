6703be816956fca32cf64649418ae8251f1fe169
ashinzekene <ashinzekene@gmail.com>
2018-02-04 04:42:19 +0100

    removing some templates

--- generators/app/app-types.js
+++ generators/app/app-types.js
@@ -1,20 +1,20 @@
 module.exports = [
-  {
-    name: 'Basic app with Interceptors and Middlewares',
-    value: '01-cats-app'
-  },
-  {
-    name: 'A Socket app with both client and server',
-    value: '02-gateways'
-  },
-  {
-    name: 'A microservices app',
-    value: '03-microservices'
-  },
-  {
-    name: 'An Injector app',
-    value: '04-injector'
-  },
+  // {
+  //   name: 'Basic app with Interceptors and Middlewares',
+  //   value: '01-cats-app'
+  // },
+  // {
+  //   name: 'A Socket app with both client and server',
+  //   value: '02-gateways'
+  // },
+  // {
+  //   name: 'A microservices app',
+  //   value: '03-microservices'
+  // },
+  // {
+  //   name: 'An Injector app',
+  //   value: '04-injector'
+  // },
   {
     name: 'SQL Typeorm',
     value: '05-sql-typeorm'
@@ -31,18 +31,15 @@ module.exports = [
     name: 'Passport',
     value: '08-passport'
   },
-  {
-    name: 'Babel JS',
-    value: '09-babel-example'
-  },
-  {
-    name: 'Mockgoose',
-    value: '10-mockgoose'
-  },
-  {
-    name: 'Swagger',
-    value: '11-swagger'
-  },
+  // {
+  //   name: 'Babel JS',
+  //   value: '09-babel-example'
+  // },
+  // {                               
+  // {
+  //   name: 'Swagger',
+  //   value: '11-swagger'
+  // },
   {
     name: 'Graph QL apollo',
     value: '12-graphql-apollo'
